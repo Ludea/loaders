4693db56c07a214411a79670b1aa49aa79591aba
Ludea <ludealol.olol@gmail.com>
2024-01-20 19:29:50 +0100

    move fb from format to util

---
+++ adt/dist/util.d.ts
@@ -0,0 +1,2 @@
+export declare const createTerrainVertexBuffer: (vertexHeights: Float32Array, vertexNormals: Int8Array) => ArrayBuffer;
+export declare const createTerrainIndexBuffer: (holes: number) => ArrayBuffer;
