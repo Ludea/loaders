82ad1e2204e6a50c773f9ca36ff7457c40167adb
Ludea <ludealol.olol@gmail.com>
2024-01-23 17:36:27 +0100

    debug

--- adt/src/Index.ts
+++ adt/src/Index.ts
@@ -3,6 +3,7 @@ import type {
   ISceneLoaderPluginFactory,
   ISceneLoaderAsyncResult,
   ISceneLoaderPlugin,
+  ISceneLoaderPluginExtensions,
   ISceneLoaderProgressEvent,
 } from "@babylonjs/core/Loading/sceneLoader";
 import type { Scene } from "@babylonjs/core/scene";
@@ -10,7 +11,7 @@ import { AssetContainer } from "@babylonjs/core/assetContainer";
 import { Mesh } from "@babylonjs/core/Meshes/mesh";
 import { MeshBuilder } from "@babylonjs/core/Meshes/meshBuilder";
 import { VertexData } from "@babylonjs/core/Meshes/mesh.vertexData";
-import { Map, MapArea } from "@wowserhq/format";
+import { MapArea } from "@wowserhq/format";
 import { createTerrainVertexBuffer, createTerrainIndexBuffer } from "util";
 
 export default class ADTFileLoader
@@ -18,8 +19,9 @@ export default class ADTFileLoader
 {
   public wdtContent: any;
   public name = "adt";
-  public extensions = ".adt";
-
+  public extensions: ISceneLoaderPluginExtensions = {
+    ".adt": { isBinary: true },
+  };
   /** @internal */
 
   public loadAssetContainerAsync(
@@ -64,12 +66,11 @@ export default class ADTFileLoader
     onProgress?: (event: ISceneLoaderProgressEvent) => void,
     fileName?: string,
   ): Promise<ISceneLoaderAsyncResult> {
+    console.log(data);
     const map = new Map().load(this.wdtContent);
-    const areaData = data;
-    const area = new MapArea(map.layerSplatDepth).load(areaData);
+    const area = new MapArea(8).load(data);
     var vertexData = new VertexData();
     var adt = new Mesh("adt", scene);
-    //let vertexBuffer;
     let indexBuffer;
     let spec: any;
     for (const chunk of area.chunks) {
@@ -82,16 +83,16 @@ export default class ADTFileLoader
       );
       indexBuffer = createTerrainIndexBuffer(chunk.holes);
     }
-    const position = new Float32Array(spec.indexBuffer!);
-    const normals = new Float32Array(spec.vertexBuffer!);
+    const position = new Float32Array(spec.indexBuffer);
+    const normals = new Float32Array(spec.vertexBuffer);
     const indices = new Uint16Array(indexBuffer!);
     vertexData.positions = position;
     vertexData.indices = indices;
     vertexData.normals = normals;
     vertexData.applyToMesh(adt);
     const option = {
-      height: spec.bounds[0],
-      width: spec.bounds[1],
+      height: 1, //spec.bounds[0],
+      width: 1, //spec.bounds[1],
     };
     const box = MeshBuilder.CreateBox("box", option, scene);
     const array: Array<Promise<void>> = [];
